ffe0c78e6523f11542c6829e714c44fa4e06858a
Bruno Henrique <brunohenriquers@2outlook.com>
2024-12-05 02:10:01 -0300

    ajustando css

--- src/sobre/Sobre.jsx
+++ src/sobre/Sobre.jsx
@@ -6,6 +6,7 @@ import reac from '../assets/icones/react.png'
 import node from '../assets/icones/node.png'
 import js from '../assets/icones/js.png'
 import bootstrap from '../assets/icones/botstrap.png'
+import sql from '../assets/icones/sql.png'
 
 
 export default function Sobre() {
@@ -17,7 +18,7 @@ export default function Sobre() {
           </p>
           <div className="titulohab">
               <img src={cerebro} alt="cerebro" className='cerebro'/> 
-              <h1 className='h1'>Conhecimentos / Habilidades </h1>
+              <h1 className='h1'>Conhecimentos - Habilidades </h1>
           </div>
           <div className="habilidades">
             
@@ -29,6 +30,7 @@ export default function Sobre() {
             <img src={node} alt="node"  className='ticon'/>
             <img src={js} alt="js"  className='ticon' />
             <img src={bootstrap} alt="bootstrap"  className='ticon'/>
+            <img src={sql} alt="bootstrap"  className='ticon'/>
           </div>
           </div>
     </div>
